83f4e6c5f84119c88b8d19b47d7b3aa66badee27
0ALEX-2 <sakilahmed965@gmail.com>
2024-05-25 00:20:07 +0530

    search page

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import { useRouter } from "next/router";
 import React from "react";
 import { format } from "date-fns";
+import InfoCard from "@/components/InfoCard";
 
 const Search = ({ searchResults }) => {
   const router = useRouter();
@@ -19,15 +20,35 @@ const Search = ({ searchResults }) => {
       />
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className="text-xs">300+ Stays for 5 number of gusts</p>
-          <h1 className="text-3xl font-semibold mt-2">Stays in Mars</h1>
+          <p className="text-xs">
+            300+ Stays for {numberOfGusts} number of gusts
+          </p>
+          <h1 className="text-3xl font-semibold mt-2">Stays in {location}</h1>
 
-          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap mt-5">
             <p className="button">Cancellation Flexibility</p>
             <p className="button">Type of Place</p>
             <p className="button">Price</p>
             <p className="button">Rooms and Beds</p>
             <p className="button">More filters</p>
+          </div>
+          <div>
+            {Array.isArray(searchResults) &&
+              searchResults.length > 0 &&
+              searchResults.map((elm, index) => {
+                return (
+                  <InfoCard
+                    key={index}
+                    img={elm.img}
+                    location={elm.location}
+                    title={elm.title}
+                    description={elm.description}
+                    star={elm.star}
+                    price={elm.price}
+                    total={elm.total}
+                  />
+                );
+              })}
           </div>
         </section>
       </main>
