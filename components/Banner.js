3ca1e3a826b27f6ef35dff702ee8b6ef763cd146
0ALEX-2 <sakilahmed965@gmail.com>
2024-05-26 13:44:32 +0530

    updated

--- components/Banner.js
+++ components/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
       <Image
-        src="/https://links.papareact.com/0fm" // Update the src to remove the full URL
+        src="/https://links.papareact.com/0fm"
         layout="fill"
         objectFit="cover"
         alt="Cover Photo"
@@ -13,7 +13,7 @@ const Banner = () => {
       <div className="absolute top-1/2 text-center w-full">
         <p className="text-sm lg:text-lg">Not sure where to go? Perfect.</p>
         <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-lg font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
-          I'm flexible
+          I am flexible
         </button>
       </div>
     </div>
